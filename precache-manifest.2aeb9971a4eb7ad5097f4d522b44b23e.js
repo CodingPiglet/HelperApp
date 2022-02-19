7f6124e3777e2d36991e5a6f595f8231e046b809
Coding Piglet <ian.kuon+app@gmail.com>
2022-02-19 17:34:09 -0500

    Updates

---
+++ precache-manifest.2aeb9971a4eb7ad5097f4d522b44b23e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79da14241b8e3e0da3cf8e7c90d47fd5",
+    "url": "/HelperApp/index.html"
+  },
+  {
+    "revision": "b5a613aa5f74ed82a0c6",
+    "url": "/HelperApp/static/css/main.95f2a87f.chunk.css"
+  },
+  {
+    "revision": "38b89dd2af27b1104974",
+    "url": "/HelperApp/static/js/2.8f2ee4b4.chunk.js"
+  },
+  {
+    "revision": "435881bb28fec4d4e84baf9dfd194218",
+    "url": "/HelperApp/static/js/2.8f2ee4b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "273dbf66fa273b7cec32",
+    "url": "/HelperApp/static/js/3.767bc4eb.chunk.js"
+  },
+  {
+    "revision": "b5a613aa5f74ed82a0c6",
+    "url": "/HelperApp/static/js/main.1e75dae8.chunk.js"
+  },
+  {
+    "revision": "39da90770cabaa8d8878",
+    "url": "/HelperApp/static/js/runtime-main.c86d57a7.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/HelperApp/static/media/logo.06e73328.svg"
+  }
+]);
